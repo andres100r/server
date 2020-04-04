5192be34cb5faf2370a72f800011adb7771f20b3
andres felipe <andres100r@hotmail.com>
2020-04-04 01:34:30 -0500

    graphql

--- src/app.js
+++ src/app.js
@@ -2,8 +2,22 @@ const path = require('path');
 const express = require('express');
 const morgan = require('morgan');
 const mongoose = require('mongoose');
-
+const express_graphql = require('express-graphql');
+const { buildSchema } = require('graphql');
 const app = express();
+const schema = buildSchema(`
+  type Query {
+    message: String
+  }
+  
+  `);
+const root = {
+  message: () => "hello mundo"
+}
+
+
+
+
 
 // conexion a mongoDB
 mongoose.connect('mongodb://localhost/arquitecrutadb')
@@ -23,6 +37,12 @@ app.use(morgan('dev'));
 app.use(express.urlencoded({extended: false}))
 // rutas
 app.use('/', indexRoutes);
+app.use('/graphql', express_graphql({
+  schema: schema,
+  rootValue: root,
+  graphiql: true
+}));
+
 
 app.listen(app.get('port'), () => {
   console.log(`server on port ${app.get('port')}`);
